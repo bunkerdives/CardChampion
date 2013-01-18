e1e6da6ca44d52e977fb856a19398e59a01d9182
alexbasalyga <alexander.basalyga@gmail.com>
2013-01-18 16:16:52 -0500

    Started adding content to #control-bar in 'draft.jade', changed the title in 'layout.jade', removed nav links from 'header.jade'

--- public/js/draft_ui.js
+++ public/js/draft_ui.js
@@ -57,24 +57,33 @@ $(window).resize(function() {
 });
 
 //Background image (i.e., card) size
-//onLoad first row of pack
+//onLoad first row of picks
 $(document).ready(function(){
 	$('#pick-row-loaded').load(function(){
 		var fixed_height = 0
 		var fixed_width = 0
 	  fixed_height = $('#pick-row-loaded').height()
 		fixed_width = $('#pick-row-loaded').width()
-		var i = 4*10 //i.e., row# * 10
+		var i = 0 //i.e., row#, 0=1
+		
 		//-0(row)-0(column)
-		function loop(n) {
-			if(n > 0)
+		function loopRow(r) {
+			if(r <= i)
 			{
-				$("#pick-card-" + n).css({
-		  		"background-size": fixed_width + "px " + fixed_height + "px"
-				}, loop(n-1));
+				
+				function loopCol(c) {
+					if(c <= 9)
+					{
+						$("#pick-card-" + r + "-" + c).css({
+				  		"background-size": fixed_width + "px " + fixed_height + "px"
+						}, loopCol(c+1));
+					}
+					loopRow(r+1)
+				}
+				loopCol(0);
 			}
 		}
-		loop(i);
+		loopRow(0);
 	});
 });
 
