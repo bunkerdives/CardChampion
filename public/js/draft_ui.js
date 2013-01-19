e9a1375422903840b21566a77ee8ebd0e0667969
alexbasalyga <alexander.basalyga@gmail.com>
2013-01-18 23:06:37 -0500

    Rounded corner for the preview image added.

--- public/js/draft_ui.js
+++ public/js/draft_ui.js
@@ -1,61 +1,73 @@
-//Pick screen height
-//onLoad of last card in pack screen
+//Pick screen top padding
+	//onLoad of last card in pack screen
 $(document).ready(function(){
-	$('#pack-row2-loaded').load(function(){
-		var fixed_height = 0
-	  fixed_height = $('#pack-screen').height()
-		fixed_height = fixed_height + 15 //i.e., padding
+	$('#pack-tran-16').load(function(){
+	  fixed_height = $('#pack-screen').height();
+		fixed_height = fixed_height + 15; //i.e., padding
 		$("#pick-screen").css({
-		  "padding-top":fixed_height + "px"
+		  "padding-top" : fixed_height + "px"
 		});
 	});
 });
 
-//onResize of window
+	//onResize of window
 $(window).resize(function() {
-	var fixed_height = 0
-  fixed_height = $('#pack-screen').height()
-	fixed_height = fixed_height + 15 //i.e., padding
+  fixed_height = $('#pack-screen').height();
+	fixed_height = fixed_height + 15; //i.e., padding
 	$("#pick-screen").css({
-	  "padding-top":fixed_height + "px"
+	  "padding-top" : fixed_height + "px"
 	});
 });
 
-//Background image (i.e., card) size
-//onLoad first row of pack
+//Background image (i.e., card) size of pack screen
+	//Pack cards
+		//onLoad first row of pack
 $(document).ready(function(){
-	$('#pack-row1-loaded').load(function(){
-		var fixed_height = 0
-		var fixed_width = 0
-	  fixed_height = $('#pack-row1-loaded').height()
-		fixed_width = $('#pack-row1-loaded').width()
-		function loop(n) {
-			if(n > 0)
-			{
-				$("#pack-card-" + n).css({
-		  		"background-size": fixed_width + "px " + fixed_height + "px"
-				}, loop(n-1));
-			}
+	$('#pack-tran-8').load(function(){
+	  fixed_height = $('#pack-tran-8').height();
+		fixed_width = $('#pack-tran-8').width();
+		for( var i = 0; i < 16; ++i ){
+			var id = "#pack-card-" + i;
+			$(id).css({
+		  	"background-size": fixed_width + "px " + fixed_height + "px"
+			});
 		}
-		loop(16);
 	});
 });
 
-//onResize of window
+		//onResize of window
 $(window).resize(function() {
-	var fixed_height = 0
-	var fixed_width = 0
-  fixed_height = $('#pack-row1-loaded').height()
-	fixed_width = $('#pack-row1-loaded').width()
-	function loop(n) {
-		if(n > 0)
-		{
-			$("#pack-card-" + n).css({
-	  		"background-size": fixed_width + "px " + fixed_height + "px"
-			}, loop(n-1));
-		}
+  fixed_height = $('#pack-tran-8').height()
+	fixed_width = $('#pack-tran-8').width()
+	for( var i = 0; i < 16; ++i ){
+		var id = "#pack-card-" + i;
+		$(id).css({
+	  	"background-size": fixed_width + "px " + fixed_height + "px"
+		});
 	}
-	loop(16);
+});
+
+	//Preview
+		//onLoad
+$(document).ready(function(){
+	$('#pack-tran-8').load(function(){
+		var id = "#preview";
+	  var fixed_height = $(id).height();
+		var fixed_width = $(id).width();
+		$(id).css({
+		  "background-size": fixed_width + "px " + fixed_height + "px"
+		});
+	});
+});
+
+		//onResize of window
+$(window).resize(function() {
+	var id = "#preview";
+  fixed_height = $(id).height()
+	fixed_width = $(id).width()
+	$(id).css({
+	  "background-size": fixed_width + "px " + fixed_height + "px"
+	});
 });
 
 //Background image (i.e., card) size of pick screen
@@ -90,28 +102,28 @@ $(document).ready(function(){
 });
 
 	//onResize of window
-$(window).resize(function() {
-	var fixed_height = 0
-	var fixed_width = 0
-  fixed_height = $('#pick-row-loaded').height()
-	fixed_width = $('#pick-row-loaded').width()
-	var i = 0 //i.e., row#, 0=1
-	//-0(row)-0(column)
-	function loopRow(r) {
-		if(r <= i)
-		{
+//$(window).resize(function() {
+//	var fixed_height = 0
+//	var fixed_width = 0
+//  fixed_height = $('#pick-row-loaded').height()
+//	fixed_width = $('#pick-row-loaded').width()
+//	var i = 44 //i.e., row#, 0=1
+//	//-0(row)-0(column)
+//	function loopRow(r) {
+//		if(r <= i)
+//		{
 				
-			function loopCol(c) {
-				if(c <= 9)
-				{
-					$("#pick-card-" + r + "-" + c).css({
-			  		"background-size": fixed_width + "px " + fixed_height + "px"
-					}, loopCol(c+1));
-				}
-				loopRow(r+1)
-			}
-			loopCol(0);
-		}
-	}
-	loopRow(0);
-});
+//			function loopCol(c) {
+//				if(c <= 9)
+//				{
+//					$("#pick-card-" + r + "-" + c).css({
+//			  		"background-size": fixed_width + "px " + fixed_height + "px"
+//					}, loopCol(c+1));
+//				}
+//				loopRow(r+1)
+//			}
+//			loopCol(0);
+//		}
+//	}
+//	loopRow(0);
+//});
