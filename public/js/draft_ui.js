7b97656ba49db3f1aaaf8b2d922f4eaa209851fe
alexbasalyga <alexander.basalyga@gmail.com>
2013-01-18 20:03:06 -0500

    Removed style tags from HTML in 'views/draft.jade' and replaced with equivalent css in 'public/css/cloudmagic.css', fixed pick card bg image resize on window resize function in 'public/js/draft_ui.js'

--- public/js/draft_ui.js
+++ public/js/draft_ui.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 	$('#pack-row2-loaded').load(function(){
 		var fixed_height = 0
 	  fixed_height = $('#pack-screen').height()
+		fixed_height = fixed_height + 15 //i.e., padding
 		$("#pick-screen").css({
 		  "padding-top":fixed_height + "px"
 		});
@@ -14,6 +15,7 @@ $(document).ready(function(){
 $(window).resize(function() {
 	var fixed_height = 0
   fixed_height = $('#pack-screen').height()
+	fixed_height = fixed_height + 15 //i.e., padding
 	$("#pick-screen").css({
 	  "padding-top":fixed_height + "px"
 	});
@@ -56,8 +58,8 @@ $(window).resize(function() {
 	loop(16);
 });
 
-//Background image (i.e., card) size
-//onLoad first row of picks
+//Background image (i.e., card) size of pick screen
+	//onLoad first row of picks
 $(document).ready(function(){
 	$('#pick-row-loaded').load(function(){
 		var fixed_height = 0
@@ -87,20 +89,29 @@ $(document).ready(function(){
 	});
 });
 
-//onResize of window
+	//onResize of window
 $(window).resize(function() {
 	var fixed_height = 0
 	var fixed_width = 0
   fixed_height = $('#pick-row-loaded').height()
 	fixed_width = $('#pick-row-loaded').width()
-	var i = 4*10 //i.e., row# * 10
-	function loop(n) {
-		if(n > 0)
+	var i = 0 //i.e., row#, 0=1
+	//-0(row)-0(column)
+	function loopRow(r) {
+		if(r <= i)
 		{
-			$("#pick-card-" + n).css({
-	  		"background-size": fixed_width + "px " + fixed_height + "px"
-			}, loop(n-1));
+				
+			function loopCol(c) {
+				if(c <= 9)
+				{
+					$("#pick-card-" + r + "-" + c).css({
+			  		"background-size": fixed_width + "px " + fixed_height + "px"
+					}, loopCol(c+1));
+				}
+				loopRow(r+1)
+			}
+			loopCol(0);
 		}
 	}
-	loop(i);
+	loopRow(0);
 });
