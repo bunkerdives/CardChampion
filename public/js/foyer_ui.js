da927bacd9ec2a54d69da5cbc9d2106ce81ee35a
alexbasalyga <alexander.basalyga@gmail.com>
2013-01-24 20:13:30 -0500

    Fixed bug with hidden scroll bars in builder, added lightbox to foyer.

--- public/js/foyer_ui.js
+++ public/js/foyer_ui.js
@@ -1,7 +1,21 @@
-function showChat() {
+function showLightbox() {
 	$('#chat-hidden').css("display", "none");
-	$('#screenshot-carousel').css("display", "none");
-	$('#chat-shown').css("display", "block");
+	$('#nickname-lightbox').css("display", "block");
+	//$('#chat-shown').css("display", "block");
+}
+
+function hideLightbox() {
+	$('#chat-hidden').css("display", "block");
+	$('#nickname-lightbox').css("display", "none");
+}
+
+function lightboxSize() {
+	var windowHeight = $(window).height();
+	var windowWidth = $(window).width();
+	$('#nickname-lightbox').css({
+		"height": windowHeight,
+		"width": windowWidth
+	});
 }
 
 
@@ -9,4 +23,9 @@ $(document).ready(function(){
   $('#screenshot-carousel').carousel({
 		interval: 5000
 	});
+	lightboxSize();
+});
+
+$(window).resize(function() {
+	lightboxSize();
 });
