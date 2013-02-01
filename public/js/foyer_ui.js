09738a5ffbce56fc1ccc55345a2a423384738dec
alexbasalyga <alexander.basalyga@gmail.com>
2013-02-01 17:02:29 -0500

    Chat scroll functionality fixed in foyer, and scaffolding altered. Add land button style tweaked on builder.

--- public/js/foyer_ui.js
+++ public/js/foyer_ui.js
@@ -1,3 +1,5 @@
+//var i =0;
+
 function lightboxSize() {
 	var windowHeight = $(window).height();
 	var windowWidth = $(window).width();
@@ -5,6 +7,21 @@ function lightboxSize() {
 		"height": windowHeight,
 		"width": windowWidth
 	});
+}
+
+//var interval = self.setInterval(appendChat,500);
+function appendChat() {
+	var convo = $("#convo");
+	var convoInner = $("#convo-inner"); 
+	var atBottom = ( convoInner.outerHeight() - convo.scrollTop() ) <=  convo.height();
+	var msg = "<div class='chat-msg'><span class='chat-username' style='color:red;'>Username 1:</span><span>Test" + i + "</span></div>";
+	convoInner.append(msg);
+	
+	if( atBottom ){
+		convo.stop().animate({ scrollTop: convo[0].scrollHeight }, 400);
+	}
+
+	//++i;
 }
 
 
