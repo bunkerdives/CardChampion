6058a6a56d5c0ff1c9795cbccfd6d31d0b818864
alexbasalyga <alexander.basalyga@gmail.com>
2013-05-03 02:51:11 -0400

    Finished mockup of functional components for the New Sealed Event subview.

--- dev/public/viewcontrollers/foyer_ui.js
+++ dev/public/viewcontrollers/foyer_ui.js
@@ -1,25 +1,147 @@
 function showCustomSealedForm() {
+	
 	console.log('showCustomSealedForm function called.');
+	
 	var $menuRight = $('#sealed-menu-right');
 	var $menuLeft = $('#sealed-menu-left');
+	var $startBtn = $('#sealed-start-btn');
+	var $selectBtn = $('#sealed-select-btn');
+	var $customBtn = $('#sealed-custom-btn');
+	var $backBtn = $('#sealed-back-btn');
 	
-	$menuRight.animate( {
-		'margin-left' : 0
-	}
-	, 500
-	, function() {
-		$menuRight.css('margin-right', 0);
-		$menuRight.addClass('pull-left');
-		$menuLeft.css('width', '50%');
-		$menuLeft.animate( {
-			'opacity' : '1'
+	$startBtn
+		.stop()
+		.animate( {
+			'opacity' : '0'
+		}
+		, 250
+		, 'linear'
+		, function () {
+			$startBtn.hide();
+			$selectBtn
+				.show()
+				.stop()
+				.animate( {
+					'opacity' : '1'
+				}
+				, 250
+				, 'linear' 
+			);
+		} );
+	
+	$customBtn
+		.stop()
+		.animate( {
+			'opacity' : '0'
+		}
+		, 250
+		, 'linear'
+		, function () {
+			$customBtn.hide();
+			$backBtn
+				.show()
+				.stop()
+				.animate( {
+					'opacity' : '1'
+				}
+				, 250
+				, 'linear'
+			);
+		} );
+	
+	$menuLeft
+		.stop()
+		.animate( {
+			'margin-left' : 0
 		}
 		, 500
+		, function() {
+			$menuLeft
+				.css('margin-right', 0)
+				.addClass('pull-left');
+			$menuRight
+				.css('width', '50%')
+				.stop()
+				.animate( {
+					'opacity' : '1'
+				}
+				, 300
+				, 'linear' );
+		} );
+}
+
+function hideCustomSealedForm() {
+	
+	console.log('hideCustomSealedForm function called.');
+	
+	var $menuRight = $('#sealed-menu-right');
+	var $menuLeft = $('#sealed-menu-left');
+	var $startBtn = $('#sealed-start-btn');
+	var $selectBtn = $('#sealed-select-btn');
+	var $customBtn = $('#sealed-custom-btn');
+	var $backBtn = $('#sealed-back-btn');
+	
+	$menuRight
+		.stop()
+		.animate( {
+			'opacity' : '0'
+		}
+		, 300
 		, 'linear'
 		, function() {
 			
+			$selectBtn
+				.stop()
+				.animate( {
+					'opacity' : '0'
+				}
+				, 250
+				, 'linear'
+				, function () {
+					$selectBtn.hide();
+					$startBtn
+						.show()
+						.stop()
+						.animate( {
+							'opacity' : '1'
+						}
+						, 250
+						, 'linear' );
+				} );
+	
+			$backBtn
+				.stop()
+				.animate( {
+					'opacity' : '0'
+				}
+				, 250
+				, 'linear'
+				, function () {
+					$backBtn.hide();
+					$customBtn
+						.show()
+						.stop()
+						.animate( {
+							'opacity' : '1'
+						}
+						, 250
+						, 'linear' );
+				} );
+			
+			$menuRight.css('width', '0%');
+			var margin = ($('#sealed-menu-inner').width()-$('#sealed-menu-left').width())/2;
+			console.log(margin)
+			$menuLeft
+				.removeClass('pull-left')
+				.stop()
+				.animate( {
+					'margin-left' : margin + 'px'
+					, 'margin-right' : margin + 'px'
+				}
+				, 500
+				, 'linear' );
+		
 		} );
-	} );
 }
 
 function profileLayout() {
